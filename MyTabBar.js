4304b263d25389e50ab7cf0e2023cdc612f82e79
Eid Almutairi <eidalmutairi0@gmail.com>
2023-03-17 05:00:03 +0300

    Adding Screen to each tab.

--- MyTabBar.js
+++ MyTabBar.js
@@ -39,90 +39,103 @@ const MyTabBar = (props) => {
   const scaleIcon = useRef(new Animated.Value(1)).current;
 
   return (
-    <SafeAreaView style={styles.tabBar}>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
       <View
         style={{
           flex: 1,
         }}
       >
-        <FlatList
-          horizontal={true}
-          scrollEnabled={false}
-          style={styles.tabs}
-          data={props.tabs}
-          renderItem={(item) => (
-            <Pressable
-              onPress={() => {
-                handleTabPressed(item.index);
-              }}
-            >
-              <SingleTab
-                width={tabWidth.toFixed()}
-                icon={item.item.icon}
-                label={item.item.label}
-                currentTab={currentTab}
-                index={item.index}
-              ></SingleTab>
-            </Pressable>
-          )}
-        ></FlatList>
+        {props.tabs[currentTab].screen}
       </View>
-      <View
-        style={{
-          flex: 1,
-          position: "absolute",
-
-          width: Dimensions.get("screen").width - 20,
-          alignSelf: "center",
-          zIndex: 200,
-        }}
-      >
-        <Animated.View
+      <SafeAreaView style={styles.tabBar}>
+        <View
           style={{
-            position: "absolute",
-            width: tabWidth,
-            transform: [
-              {
-                translateX: changeTab,
-              },
-            ],
-
-            alignItems: "center",
+            flex: 1,
           }}
         >
-          <View
-            style={{
-              width: 48,
-              height: 48,
-              borderRadius: 10,
-              backgroundColor: "rgb(0 160 138)",
-              alignContent: "center",
-              alignItems: "center",
+          <FlatList
+            horizontal={true}
+            scrollEnabled={false}
+            style={styles.tabs}
+            data={props.tabs}
+            renderItem={(item) => (
+              <Pressable
+                onPress={() => {
+                  handleTabPressed(item.index);
+                }}
+              >
+                <SingleTab
+                  width={tabWidth.toFixed()}
+                  icon={item.item.icon}
+                  label={item.item.label}
+                  currentTab={currentTab}
+                  index={item.index}
+                ></SingleTab>
+              </Pressable>
+            )}
+          ></FlatList>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            position: "absolute",
 
-              top: -15,
-              zIndex: 200,
-              justifyContent: "center",
+            width: Dimensions.get("screen").width - 20,
+            alignSelf: "center",
+            zIndex: 200,
+          }}
+        >
+          <Animated.View
+            style={{
+              position: "absolute",
+              width: tabWidth,
+              transform: [
+                {
+                  translateX: changeTab,
+                },
+              ],
+
+              alignItems: "center",
             }}
           >
-            <Animated.View
+            <View
               style={{
-                transform: [
-                  {
-                    scale: scaleIcon,
-                  },
-                ],
+                width: 48,
+                height: 48,
+                borderRadius: 10,
+                backgroundColor: "rgb(0 160 138)",
+                alignContent: "center",
+                alignItems: "center",
+
+                top: -15,
+                zIndex: 200,
+                justifyContent: "center",
               }}
             >
-              <Icon
-                style={{ color: "white" }}
-                name={props.tabs[currentTab].icon}
-                size={22}
-              ></Icon>
-            </Animated.View>
-          </View>
-        </Animated.View>
-      </View>
-    </SafeAreaView>
+              <Animated.View
+                style={{
+                  transform: [
+                    {
+                      scale: scaleIcon,
+                    },
+                  ],
+                }}
+              >
+                <Icon
+                  style={{ color: "white" }}
+                  name={props.tabs[currentTab].icon}
+                  size={22}
+                ></Icon>
+              </Animated.View>
+            </View>
+          </Animated.View>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
@@ -130,15 +143,14 @@ export default MyTabBar;
 
 const styles = StyleSheet.create({
   tabBar: {
-    flex: 1,
     color: "white",
     overflow: "visible",
     width: Dimensions.get("screen").width,
     backgroundColor: "white",
     height: 90,
-    bottom: 0,
+    position: "relative",
     zIndex: 10,
-    position: "absolute",
+
     shadowColor: "#171717",
     shadowOffset: { width: -2, height: 4 },
     shadowOpacity: 0.5,
